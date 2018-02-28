4e19a57373471d846971f3c2790bd397f8bfafdc
gnerv <34653480+gnerv@users.noreply.github.com>
2018-02-28 22:27:29 +0800

    123

--- pages/detail/detail.js
+++
@@ -1,7 +0,0 @@
-//detail.js
-//获取应用实例
-var app = getApp();
-Page({
-  data: {
-  },
-});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,31 +1,82 @@
 //index.js
 //获取应用实例
 var app = getApp();
+var dataUrl = 'http://www.gnerv.com:8080/gnerv/music/2.mp3'
 Page({
   data: {
     animationData: {},
     cardInfoList: [{
-      cardUrl: 'http://dev.wemart.cn/console/images/card/card3.png',
-      cardInfo: {
-        cardTitle: '你不知道的花事',
-        cardInfoMes: ['一月一送，浪漫节日送浪漫花', '一月两送，有趣节日送奇异花', '一月四送，有你在每天都是最好的节日']
+        cardUrl: "../../static/image/1 (1).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (2).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (3).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (4).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (5).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (6).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (7).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (8).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
+        }
+      }, {
+        cardUrl: "../../static/image/1 (9).jpg",
+        cardInfo: {
+          cardTitle: 'TO亲爱的闺蜜！！！',
+          cardInfoMes: ['亲爱的闺蜜，你永远也看不到我寂寞时候的样子，因为只有你不在我身边的时候，我才会寂寞！']
       }
-    }, {
-      cardUrl: 'http://dev.wemart.cn/console/images/card/card2.png',
-      cardInfo: {
-        cardTitle: '你不知道的花事',
-        cardInfoMes: ['一月一送，浪漫节日送浪漫花', '一月两送，有趣节日送奇异花', '一月四送，有你在每天都是最好的节日']
-      }
-    }, {
-      cardUrl: 'http://dev.wemart.cn/console/images/card/card1.png',
-      cardInfo: {
-        cardTitle: '你不知道的花事',
-        cardInfoMes: ['一月一送，浪漫节日送浪漫花', '一月两送，有趣节日送奇异花', '一月四送，有你在每天都是最好的节日']
-      }
-    }]
+    }],
+
+    current: {
+      poster: '../../static/image/1 (5).jpg',
+      name: '为爱追寻',
+      src: 'http://www.gnerv.com:8080/gnerv/music/2.mp3',
+    },
+    audioAction: {
+      method: 'pause'
+    }
+
   },
   //事件处理函数
   slidethis: function(e) {
+    var Ry = Math.floor(Math.random() * 1000 + 500);
+    var Rr = Math.floor(Math.random() * 90);
+    var Rx = Math.floor(Math.random() * 1000 + 500);
+
     console.log(e);
     var animation = wx.createAnimation({
       duration: 300,
@@ -33,8 +84,12 @@ Page({
     });
     var self = this;
     this.animation= animation;
-    this.animation.translateY(-420).rotate(-5).translateX(0).step();
-    this.animation.translateY(62).translateX(25).rotate(0).step();
+    if (Ry < 1000){
+      this.animation.translateY(Ry).rotate(Rr).translateX(Rx).step();
+    }  else{
+      this.animation.translateY(-Ry).rotate(-Rr).translateX(-Rx).step();
+    }
+    this.animation.translateY(60).translateX(25).rotate(0).step();
     this.setData({
       animationData: this.animation.export()
     });
@@ -48,22 +103,98 @@ Page({
       });
     }, 350);
   },
-  buythis: function(e) {
-    console.log(e);
-    app.buyDetail = this.data.cardInfoList[e.target.id];
-    wx.navigateTo({
-      url: '../detail/detail'
-    });
-  },
+  // buythis: function(e) {
+  //   console.log(e);
+  //   app.buyDetail = this.data.cardInfoList[e.target.id];
+  //   wx.navigateTo({
+  //     url: '../detail/detail'
+  //   });
+  // },
+
+
+
   onLoad: function () {
-    console.log('onLoad');
-    var that = this;
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      });
-    });
+    this._enableInterval()
+
+    if (app.globalData.backgroundAudioPlaying) {
+      this.setData({
+        playing: true
+      })
+    }
+    this.play()
+  },
+
+  play: function (res) {
+    var that = this
+    wx.playBackgroundAudio({
+      dataUrl: dataUrl,
+      title: '此时此刻',
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      complete: function (res) {
+        that.setData({
+          playing: true
+        })
+      }
+    })
+    this._enableInterval()
+    app.globalData.backgroundAudioPlaying = true
+  },
+  seek: function (e) {
+    clearInterval(this.updateInterval)
+    var that = this
+    wx.seekBackgroundAudio({
+      position: e.detail.value,
+      complete: function () {
+        // 实际会延迟两秒左右才跳过去
+        setTimeout(function () {
+          that._enableInterval()
+        }, 2000)
+      }
+    })
+  },
+  pause: function () {
+    var that = this
+    wx.pauseBackgroundAudio({
+      dataUrl: dataUrl,
+      success: function () {
+        that.setData({
+          playing: false
+        })
+      }
+    })
+    app.globalData.backgroundAudioPlaying = false
+  },
+  stop: function () {
+    var that = this
+    wx.stopBackgroundAudio({
+      dataUrl: dataUrl,
+      success: function (res) {
+        that.setData({
+          playing: false,
+          playTime: 0,
+          formatedPlayTime: util.formatTime(0)
+        })
+      }
+    })
+    app.globalData.backgroundAudioPlaying = false
+  },
+  _enableInterval: function () {
+    var that = this
+    update()
+    this.updateInterval = setInterval(update, 500)
+    function update() {
+      wx.getBackgroundAudioPlayerState({
+        success: function (res) {
+          that.setData({
+            playTime: res.currentPosition,
+            formatedPlayTime: util.formatTime(res.currentPosition + 1)
+          })
+        }
+      })
+    }
+  },
+  onUnload: function () {
+    clearInterval(this.updateInterval)
   }
+
 })
